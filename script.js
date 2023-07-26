08ed9b7dede504c2e54f1c8f975fe5b43e69176f
Lisa <95229586+LisaKolesnik@users.noreply.github.com>
2023-07-26 09:58:54 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
+let flag = 0;
+let dpdClose = document.getElementById("dropdown");
+let closeBg = document.querySelector(".background");
 
-function openClose(x) {
-  x.classList.toggle("change");
-  document.getElementById("dropdown").classList.toggle("show");
+function openClose() {
+  if (flag === 0) {
+    document.querySelector(".dropdown-btn").classList.toggle("change");
+    dpdClose.classList.toggle("show");
+    document.body.style.overflowY = "hidden";
+    closeBg.style.display = "block";
+    flag = 1;
+  } else {
+    document.querySelector(".dropdown-btn").classList.remove("change");
+    dpdClose.classList.remove("show");
+    document.body.style.overflowY = "scroll";
+    closeBg.style.display = "none";
+    flag = 0;
+  }
 }
 
-// window.onclick = function(event) {
-//   if (!event.target.matches('.dropdown-btn')) {
-//     var dropdowns = document.getElementsByClassName("dropdown-content");
-//     var i;
-//     for (i = 0; i < dropdowns.length; i++) {
-//       var openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains('show')) {
-//         openDropdown.classList.remove('show');
-//       }
-//     }
-//   }
-// }
+window.onclick = function (event) {
+  if (event.target == closeBg) {
+    document.querySelector(".dropdown-btn").classList.remove("change");
+    document.getElementById("dropdown").classList.remove("show");
+    document.body.style.overflowY = "scroll";
+    closeBg.style.display = "none";
+    flag = 0;
+  }
+};
 
 function openSkill(event, skillName, stat) {
   var i, tabcontent, tablinks;
@@ -58,20 +69,79 @@ function openTab(evt, tabName) {
   evt.currentTarget.className += " active";
 }
 
-var modal = document.getElementById("mod");
 
-var img = document.getElementById("img");
-var modalImg = document.getElementById("image");
-img.onclick = function() {
-  modal.style.display = "block";
-  modalImg.src = this.src;
-};
+fetch('./data.json')
+  .then((response) => response.json())
+  .then((json) => {
+    var arr = Array(20);
 
-var span = document.getElementsByClassName("close")[0];
+    for (var i = 0; i < arr.length; i++) {
+      let div = document.querySelector(".modal-div");
+      const card = document.createElement("div");
+      card.classList.add("modal");
+      card.setAttribute("data-modal", json[i].id);
+      const img = document.createElement("img");
+      img.classList.add("modal-content");
+      img.src = json[i].img;
+      const link = document.createElement("a");
+      link.classList.add("modal-link");
+      link.classList.add("disable");
+      link.href = json[i].link;
+      link.target = "_blank";
+      const button = document.createElement("button");
+      button.classList.add("close");
+      card.append(link);
+      link.append(img);
+      card.append(button);
+      div.append(card);
+    }
 
-span.onclick = function() {
-  modal.style.display = "none";
-};
+    enable = document.querySelectorAll(".modal-link");
+    for (let j = 0; j < 20; j++) {
+      if (j == 0 || j == 2 || j == 7 || j == 8 || j == 9) {
+        enable[j].classList.remove("disable");
+      }
+    }
+
+    small = document.querySelectorAll(".modal-content");
+    for (let k = 0; k < 20; k++) {
+      if (k == 3 || k == 4) {
+        small[k].classList.add("small");
+      }
+    }
+
+    middle = document.querySelectorAll(".modal-content");
+    for (let r = 0; r < 20; r++) {
+      if (r == 16 || r == 17 || r == 18 || r == 19) {
+        middle[r].classList.add("middle");
+      }
+    }
+
+    let btns = document.querySelectorAll('.portfolio-img');
+    btns.forEach((el) => {
+      el.addEventListener('click', (e) => {
+        let path = e.currentTarget.getAttribute('data-path');
+
+        modals.forEach((el) => {
+          el.classList.remove('modal--visible');
+        });
+
+        document.querySelector(`[data-modal="${path}"]`).classList.add('modal--visible');
+        document.body.style.overflowY = "hidden";
+      });
+    });
 
 
+    let modals = document.querySelectorAll('.modal');
+    let close = document.querySelectorAll(".close");
+
+    close.forEach(button => {
+      button.onclick = function () {
+        modals.forEach((el) => {
+          el.classList.remove('modal--visible');
+        });
+        document.body.style.overflowY = "scroll";
+      };
+    });
+  });
 
